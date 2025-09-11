7df01cb2b6c8bd2b39bb7bff9228243925e54a21
vikaruma <vishnukarumanchi1@gmail.com>
2025-09-11 14:33:26 +0530

    testing

---
+++ src/main/java/com/example/demo/DTO/BondCreationDto.java
@@ -0,0 +1,68 @@
+package com.example.demo.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+public class BondCreationDto {
+    @JsonProperty("customerId")
+    private int customerId;
+    @JsonProperty("liabilityType")
+    private String liabilityType;
+    @JsonProperty("liabilityValue")
+    private double liabilityValue;
+    @JsonProperty("annualInterestRate")
+    private double annualInterestRate;
+    @JsonProperty("interestPayBackDate")
+    private LocalDate interestPayBackDate;
+    @JsonProperty("maturityDate")
+    private LocalDate maturityDate;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getLiabilityType() {
+        return liabilityType;
+    }
+
+    public void setLiabilityType(String liabilityType) {
+        this.liabilityType = liabilityType;
+    }
+
+    public double getLiabilityValue() {
+        return liabilityValue;
+    }
+
+    public void setLiabilityValue(double liabilityValue) {
+        this.liabilityValue = liabilityValue;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public LocalDate getInterestPayBackDate() {
+        return interestPayBackDate;
+    }
+
+    public void setInterestPayBackDate(LocalDate interestPayBackDate) {
+        this.interestPayBackDate = interestPayBackDate;
+    }
+
+    public LocalDate getMaturityDate() {
+        return maturityDate;
+    }
+
+    public void setMaturityDate(LocalDate maturityDate) {
+        this.maturityDate = maturityDate;
+    }
+}
